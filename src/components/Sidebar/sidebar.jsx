98baab618256da1549cf29244191f1bd8706d1ba
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-20 11:06:42 -0500

    sidebar menu options in list and colors added to page

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,11 +1,25 @@
 import "./sidebar.css"
+import {LineStyle, Message, ListAlt} from "@material-ui/icons"
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="sidebarwrapper">
-                <div className="sidebarmenu">
-                    <h4 className="title">Your Items</h4>
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <LineStyle />
+                        Home
+                    </li>
+                    <li className="sidebarListItem">
+                        <Message />
+                        Messages
+                    </li>
+                    <li className="sidebarListItem">
+                        <ListAlt />
+                        To-Do
+                    </li>
+                    </ul>
                 </div>
             </div>
         </div>
