7e53794778ea9f337dcc9b0fb7c577e992594c71
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-20 18:23:09 -0500

    styling side bar

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -3,25 +3,25 @@ import {LineStyle, Message, ListAlt} from "@material-ui/icons"
 
 export default function Sidebar() {
     return (
-        <div className="sidebar">
-            <div className="sidebarWrapper">
-                <div className="sidebarMenu">
-                    <ul className="sidebarList">
-                    <li className="sidebarListItem">
-                        <LineStyle />
-                        Home
-                    </li>
-                    <li className="sidebarListItem">
-                        <Message />
-                        Messages
-                    </li>
-                    <li className="sidebarListItem">
-                        <ListAlt />
-                        To-Do
-                    </li>
-                    </ul>
-                </div>
-            </div>
+      <div className="sidebar">
+        <div className="sidebarWrapper">
+          <div className="sidebarMenu">
+            <ul className="sidebarList">
+              <li className="sidebarListItem">
+                <LineStyle classname="sideIcon" />
+                Home
+              </li>
+              <li className="sidebarListItem">
+                <Message classname="sideIcon" />
+                Messages
+              </li>
+              <li className="sidebarListItem">
+                <ListAlt classname="sideIcon" />
+                To-Do
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
