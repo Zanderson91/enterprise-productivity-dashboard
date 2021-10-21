3887266559c596e623beb874838a2c39699682b9
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-21 08:59:43 -0500

    adding second row on homepage for some sort of data

--- src/Pages/Home/homepage.jsx
+++ src/Pages/Home/homepage.jsx
@@ -1,10 +1,12 @@
 import './homepage.css'
 import Card from '../../components/Card/card'
+import Graph from '../../components/Graph/graph'
 
 export default function Homepage() {
     return (
         <div className="homepage">
             <Card />
+            <Graph />
         </div>
     )
 }
