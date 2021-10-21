eaf02f458302c03cacce83c38fd6f23fffe08e64
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-21 11:08:04 -0500

    added sidebartest for class later

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/Sidebar/sidebar";
 import "./app.css";
 import Homepage from "./Pages/Home/homepage";
+import GmailSidebarItem from "./components/sidebarTest/sidebarTest";
 
 function App() {
   return (
