3a6fc3c66bbbdce24a237606f5264dc2aa3ab68d
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-21 08:44:17 -0500

    card displaying with shadow on page

--- src/Pages/Home/homepage.jsx
+++ src/Pages/Home/homepage.jsx
@@ -1,9 +1,10 @@
 import './homepage.css'
+import Card from '../../components/Card/card'
 
 export default function Homepage() {
     return (
         <div className="homepage">
-            
+            <Card />
         </div>
     )
 }
--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,11 +1,29 @@
-import './card'
+import "./card.css";
 
 export default function Card() {
   return (
     <div className="card">
       <div className="cardItem">
-      <span className="cardTitle"></span>
+        <span className="cardTitle"> Test Card 1</span>
+        <div className="cardContainer">
+          <span className="info1"> Information Here</span>
+        </div>
+        <span className="bodyText"> Information about stuff..</span>
+      </div>
+      <div className="cardItem">
+        <span className="cardTitle"> Test Card 2</span>
+        <div className="cardContainer">
+          <span className="info1"> Information Here</span>
+        </div>
+        <span className="bodyText"> Information about stuff..</span>
+      </div>
+      <div className="cardItem">
+        <span className="cardTitle"> Test Card 3</span>
+        <div className="cardContainer">
+          <span className="info1"> Information Here</span>
+        </div>
+        <span className="bodyText"> Information about stuff..</span>
       </div>
     </div>
-  )
+  );
 }
