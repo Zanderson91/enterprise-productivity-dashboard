c917cab073c96e46ac96d1779c758b50cf101f6f
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-21 21:32:22 -0500

    Updated nav settings

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import Sidebar from "./components/Sidebar/sidebar";
 import "./app.css";
 import Homepage from "./Pages/Home/homepage";
 import GmailSidebarItem from "./components/sidebarTest/sidebarTest";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <div className="container">
-        <Sidebar />
-        <Homepage />
+      <div className="App">
+        <div className="container">
+          <Sidebar />
+          <Homepage />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
