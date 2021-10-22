92a6227e391f24b93f061b9aa74307bf94193baa
Anthony Cromartie <acromarti@gmail.com>
2021-10-21 19:26:06 -0500

    imported weatherAPI functions from utils into weatherCard.jsx component

--- src/components/Card/WeatherCard/weatherCard.jsx
+++ src/components/Card/WeatherCard/weatherCard.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import "../card.css";
 
+import { getLongitudeAndLatitude, getWeatherStats } from "../../../utils/weatherAPI";
+
 export default function WeatherCard() {
     return (
         <div className="cardItem">
