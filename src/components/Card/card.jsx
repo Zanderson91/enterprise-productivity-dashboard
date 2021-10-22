64c1d985cd726aaf1a9d24a6beffa660f1046a59
Anthony Cromartie <acromarti@gmail.com>
2021-10-21 19:49:52 -0500

    fixed import errors in card.jsx

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,15 +1,10 @@
 import "./card.css";
+import WeatherCard from "./WeatherCard/weatherCard";
 
 export default function Card() {
   return (
     <div className="card">
-      <div className="cardItem">
-        <span className="cardTitle"> Test Card 1</span>
-        <div className="cardContainer">
-          <span className="info1"> Information Here</span>
-        </div>
-        <span className="bodyText"> Information about stuff..</span>
-      </div>
+      <WeatherCard />
       <div className="cardItem">
         <span className="cardTitle"> Test Card 2</span>
         <div className="cardContainer">
