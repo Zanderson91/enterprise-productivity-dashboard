71ffddb436adc9911852f9a5fa901a229f8c3e62
Anthony Cromartie <acromarti@gmail.com>
2021-10-21 19:11:13 -0500

    added weather api getLongitudeAndLatitude to utils

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/sidebar";
 import "./app.css";
 import Homepage from "./Pages/Home/homepage";
