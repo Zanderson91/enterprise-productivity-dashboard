100c1bbc6dbac88f4746674d90a6de9a10f35982
Anthony Cromartie <acromarti@gmail.com>
2021-10-21 19:28:28 -0500

    declared weather variables and useStates in weatherCard.jsx

--- src/components/Card/WeatherCard/weatherCard.jsx
+++ src/components/Card/WeatherCard/weatherCard.jsx
@@ -4,9 +4,17 @@ import "../card.css";
 import { getLongitudeAndLatitude, getWeatherStats } from "../../../utils/weatherAPI";
 
 export default function WeatherCard() {
+
+    const [input, setInput] = useState("");
+    const [cityName, setCityName] = useState("");
+    const [cityTemp, setCityTemp] = useState(0);
+    const [cityWindSpeed, setCityWindSpeed] = useState(0);
+    const [cityHumidity, setCityHumidity] = useState(0);
+    const [status, setStatus] = useState("");
+    
     return (
         <div className="cardItem">
-            
+
         </div>
     );
 }
