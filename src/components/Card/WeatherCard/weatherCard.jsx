4af1a4084fc97c369ca504723834672415ca37cc
Anthony Cromartie <acromarti@gmail.com>
2021-10-21 19:24:25 -0500

    added weatherCard.jsx component

---
+++ src/components/Card/WeatherCard/weatherCard.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import "../card.css";
+
+export default function WeatherCard() {
+    return (
+        <div className="cardItem">
+            
+        </div>
+    );
+}
