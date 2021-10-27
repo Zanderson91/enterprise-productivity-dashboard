763de106d0ddf01084243f6a8d22eeb4c87249d1
Anthony Cromartie <acromarti@gmail.com>
2021-10-26 20:42:19 -0500

    modified server.js to our current server.js

--- server/server.js
+++ server/server.js
@@ -5,30 +5,26 @@ const { ApolloServer } = require("apollo-server-express");
 // const routes = require('./routes');
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-
-server.applyMiddleware({ app });
-
+async function startServer() {
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+}
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`:earth_africa: Now listening on localhost:${PORT}`));
+  startServer();
 });
