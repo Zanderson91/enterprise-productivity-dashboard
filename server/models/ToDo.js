ff4eaf31bb8607769a97eea24e5f63e76464e0f9
Anthony Cromartie <acromarti@gmail.com>
2021-10-26 20:34:11 -0500

    added ToDo.js model in model folder

---
+++ server/models/ToDo.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose")
+
+const toDoSchema = Schema(
+    {
+        id: {
+            type: Number,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        eagerness: {
+            type: String
+        },
+    }
+);
+
+const ToDo = model("ToDo", toDoSchema)
+
+module.exports = ToDo;
+
