45061e3ff0c454a82e9c794fdc32542716d2cad6
Anthony Cromartie <acromarti@gmail.com>
2021-10-26 19:32:44 -0500

    added getAllToDos fetch call function in toDoAPI.js

--- client/src/utils/toDoAPI.js
+++ client/src/utils/toDoAPI.js
@@ -10,4 +10,16 @@ export const createToDo = async (item) => {
     }
     const response = await fetch("/api/toDo", options);
     await response.json();
-  };
+};
+
+export const getAllToDos = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const response = await fetch("/api/toDo/allToDos", options);
+    const data = await response.json();
+    return data;
+};
