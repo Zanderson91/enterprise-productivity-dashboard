f4a2a3b4f617e36777fdac54e2ed3ff6b3452ca4
Anthony Cromartie <acromarti@gmail.com>
2021-10-26 20:30:22 -0500

    added removeToDo function inside toDo-controller.js

--- server/controllers/toDo-controller.js
+++ server/controllers/toDo-controller.js
@@ -21,4 +21,15 @@ module.exports = {
 
     res.status(200).json(allToDos);
   },
+
+  async removeToDo(req, res) {
+    const { toDoId } = req.params;
+    const toDo = await ToDo.findOneAndDelete({id: toDoId});
+
+    if (!toDo) {
+      return res.status(400).json({ message: 'Unable to find todos with this id' });
+    }
+
+    res.status(200).json(toDo);
+  }
 };
