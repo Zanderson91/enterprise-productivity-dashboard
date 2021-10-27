848918feeedcdf29c84a729763debd7ee96e563f
Anthony Cromartie <acromarti@gmail.com>
2021-10-26 19:28:32 -0500

    added ToDoList component to card.jsx

--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -1,18 +1,13 @@
 import "./card.css";
 import WeatherCard from "./WeatherCard/weatherCard";
+import ToDoList from "./ToDoCard/toDoList";
 import { MediaControlCard } from "../musicCard/musicCard";
 
 export default function Card() {
   return (
     <div className="card">
       <WeatherCard />
-      <div className="cardItem">
-        <span className="cardTitle"> Test Card 2</span>
-        <div className="cardContainer">
-          <span className="info1"> Information Here</span>
-        </div>
-        <span className="bodyText"> Information about stuff..</span>
-      </div>
+      <ToDoList />
       <div class="music">
         <MediaControlCard />
       </div>
