3217edf0a96852bac1a5f366042277c7f4fbd71a
Anthony Cromartie <acromarti@gmail.com>
2021-10-28 18:56:28 -0500

    added updateToDo function in toDo-controller.js

--- server/controllers/toDo-controller.js
+++ server/controllers/toDo-controller.js
@@ -22,6 +22,22 @@ module.exports = {
     res.status(200).json(allToDos);
   },
 
+  async updateToDo(req, res) {
+    const item = {
+      id: req.body.id,
+      text: req.body.text,
+      eagerness: req.body.eagerness,
+      isComplete: req.body.isComplete,
+    }
+    await ToDo.findOneAndUpdate(
+      { id: req.body.id },
+      { $set: item },
+      { new: true }
+    );
+    
+    res.status(200).json({message: "Successfully updated to do"});
+  },
+
   async removeToDo(req, res) {
     const { toDoId } = req.params;
     const toDo = await ToDo.findOneAndDelete({id: toDoId});
