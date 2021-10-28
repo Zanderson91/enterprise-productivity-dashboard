58e7851334d1050463c70847b1737037411926ff
Anthony Cromartie <acromarti@gmail.com>
2021-10-28 18:53:37 -0500

    added updateToDo fetch call function in toDoAPI.js

--- client/src/utils/toDoAPI.js
+++ client/src/utils/toDoAPI.js
@@ -1,33 +1,48 @@
 // fetch calls calling the routes in server/routes/api/toDo-routes.js
 
 export const createToDo = async (item) => {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(item),
-    }
-    const response = await fetch("/api/toDo", options);
-    await response.json();
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item),
+  }
+  const response = await fetch("/api/toDo", options);
+  await response.json();
 };
 
 export const getAllToDos = async () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-    const response = await fetch("/api/toDo/allToDos", options);
-    const data = await response.json();
-    return data;
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+  const response = await fetch("/api/toDo/allToDos", options);
+  const data = await response.json();
+  return data;
 };
 
 export const removeToDo = async (toDoId) => {
-    const options = {
-      method: 'DELETE',
-    }
-    const response = await fetch(`/api/toDo/${toDoId}`, options);
-    await response.json();
+  const options = {
+    method: 'DELETE',
+  }
+  const response = await fetch(`/api/toDo/${toDoId}`, options);
+  await response.json();
 };
+
+export const updateToDo = async (item) => {
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item),
+  }
+  const response = await fetch("/api/toDo", options);
+  await response.json();
+};
+
+
+
