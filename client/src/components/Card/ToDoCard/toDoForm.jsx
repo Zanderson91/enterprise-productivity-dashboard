07cb0916f0b537840ba1630d181a58b0aaa5b63d
Anthony Cromartie <acromarti@gmail.com>
2021-10-28 19:21:22 -0500

    commented out all functions except add

--- client/src/components/Card/ToDoCard/toDoForm.jsx
+++ client/src/components/Card/ToDoCard/toDoForm.jsx
@@ -18,6 +18,7 @@ function ToDoForm({ setToDoList, edit, setEdit }) {
   useEffect(() => {
     if (addBtnIsClicked) {
       const addToDoItem = async() => {
+        console.log(toDoItem)
         await createToDo(toDoItem);
         const allToDos = await getAllToDos();
         setToDoList(allToDos);
@@ -30,16 +31,16 @@ function ToDoForm({ setToDoList, edit, setEdit }) {
   useEffect(() => {
     if (editBtnIsClicked) {
       const editToDoItem = async() => {
-        const item = {
-          id: edit.id,
-          text: toDoItem.text,
-          eagerness: toDoItem.eagerness,
-          isComplete: toDoItem.isComplete 
-        }
-        await updateToDo(item);
-        const allToDos = await getAllToDos();
-        setToDoList(allToDos);
-        setEdit({ id: null, value: '', eagerness: '' });               
+        // const item = {
+        //   id: edit.id,
+        //   text: toDoItem.text,
+        //   eagerness: toDoItem.eagerness,
+        //   isComplete: toDoItem.isComplete 
+        // }
+        // await updateToDo(item);
+        // const allToDos = await getAllToDos();
+        // setToDoList(allToDos);
+        // setEdit({ id: null, value: '', eagerness: '' });               
       }
       editToDoItem();      
       setEditBtnIsClicked(false);
